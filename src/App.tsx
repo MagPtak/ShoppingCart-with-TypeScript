04bdfb5503976a82c73da847de179f9a719b12e7
Magdalena Ptak <magdalena.ptak1990@gmail.com>
2022-03-17 18:52:23 +0100

    Fetch data from fake API

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
+import { useState } from 'react'
+import { useQuery } from 'react-query'
+import { Drawer, LinearProgress, Grid, Badge } from '@mui/material';
+// import { AddShoppingCartIcon } from "@mui/icons-material/";
+import { Wrapper } from './App.styles';
 
+export type CartItemType = {
+  id: number,
+  category: string,
+  description: string,
+  image: string,
+  price: number,
+  title: string,
+  amount: number
+}
+
+const getProducts = async (): Promise<CartItemType[]> => 
+  await (await fetch('https://fakestoreapi.com/products/')).json()
 
 function App() {
+  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts) //ReactQuery to fetch data, 'products' is a key
+  console.log(data)
   return (
     <div className="App">
       Makapaka
