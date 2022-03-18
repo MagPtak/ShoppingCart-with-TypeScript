92ddc66534f24d5d29d924578de202d578b543ed
Magdalena Ptak <magdalena.ptak1990@gmail.com>
2022-03-18 13:40:04 +0100

    Add Cart component

---
+++ src/Cart/Cart.styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.aside`
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  width: 500px;
+  padding: 20px;
+`;
